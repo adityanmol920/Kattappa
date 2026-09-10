f6e785fabf52bf685d04671655dce4eb28e87a36
Aditya Anmol <adityanmol920@outlook.com>
2026-09-11 05:21:00 +0530

    Release 0.2.2 | corrected match URL

--- scripts/kattappa.user.js
+++ scripts/kattappa.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://github.com/adityanmol920/Kattappa
 // @version      0.2.2
 // @description  Kattappa trading discipline overlay.
-// @match        https://groww.in/*
+// @match        groww.in/*
 // @require      https://raw.githubusercontent.com/adityanmol920/Kattappa/v0.2.2/dist/bundle.js
 // @updateURL    https://raw.githubusercontent.com/adityanmol920/Kattappa/v0.2.2/scripts/kattappa.user.js
 // @downloadURL  https://raw.githubusercontent.com/adityanmol920/Kattappa/v0.2.2/scripts/kattappa.user.js
